04c236efa059f2adb4b9e52800df098d81eec819
DavigamerKolling <davigalvaokolling@gmail.com>
2025-10-02 16:14:45 -0300

    Add files via upload

---
+++ BancoDeDados.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE plataforma_cursos;
+USE plataforma_cursos;
+
+CREATE TABLE materias (
+    id_materia INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE aulas (
+    id_aula INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(150) NOT NULL,
+    descricao TEXT,
+    data_aula DATE,
+    id_materia INT,
+    FOREIGN KEY (id_materia) REFERENCES materias(id_materia)
+);
+
+CREATE TABLE materiais (
+    id_material INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(150) NOT NULL,
+    tipo ENUM('slide','artigo','livro') NOT NULL,
+    arquivo VARCHAR(255), -- caminho ou nome do arquivo
+    id_materia INT,
+    FOREIGN KEY (id_materia) REFERENCES materias(id_materia)
+);
+
+CREATE TABLE eventos (
+    id_evento INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    descricao TEXT,
+    data_evento DATE,
+    id_materia INT,
+    FOREIGN KEY (id_materia) REFERENCES materias(id_materia)
+);
